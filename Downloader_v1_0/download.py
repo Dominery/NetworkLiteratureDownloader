91a2cb1b6ae35597f8e0a79aa8ba0205d1f7476f
苏宇 <54257234+Dominery@users.noreply.github.com>
2020-12-20 12:03:48 +0800

    实现多协程进行下载
    耗时311s

--- Downloader_v1_0/download.py
+++ Downloader_v1_0/download.py
@@ -1,4 +1,9 @@
 import os
+import time
+
+import gevent
+from gevent import pool,monkey
+monkey.patch_all()
 
 from Downloader_v1_0.article import Article
 from Downloader_v1_0.book import Book
@@ -8,11 +13,13 @@ class Download:
     def __init__(self, book_name):
         self.book = Book(book_name)
         self.article = Article
+        self.process = 0
 
     def get_article_urls(self):
         self.book.get_book_info()
         self.book.make_choice()
         self.book.get_urls_article()
+        self.sum_threads = len(self.book.article_urls)
 
     def mkdir(self, directory_path):
         self.directory_path = os.path.join(directory_path, self.book.book_url[1])
@@ -26,15 +33,19 @@ class Download:
         article.get_title()
         article.get_content()
         article.write(self.directory_path)
+        self.process += 1
+        format_process = '\r[{:<20}]已下载:{:.2f}%'.format('#' * (self.process * 20 // self.sum_threads),
+                                                        self.process / self.sum_threads*100)
+        print(format_process, end='')
 
     def download(self, directory_path):
+        start_time = time.time()
         self.get_article_urls()
         self.mkdir(directory_path)
-        sum_articles = len(self.book.article_urls)
-        for i in range(sum_articles):
-            self.make_article(self.book.article_urls[i])
-            format_process = '\r[{:<20}]已下载:{:.2f}%'.format('#' * (i * 20 // sum_articles), i / sum_articles)
-            print(format_process, end='')
+        Pool = pool.Pool(5)
+        gevent.joinall([Pool.spawn(self.make_article,i) for i in self.book.article_urls])
+        end_time = time.time()
+        print(f'\n共计耗时{end_time-start_time}')
 
 
 def main():
